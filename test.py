3e27a6d95fda32999c298c9f77233a6af7ee8cec
Zavioer <piotrekq321@gmail.com>
2020-05-26 23:34:42 +0200

    Improve test.py file and add new module bookshelf.
    Module bookshelf contains Bookshelf object with unit tests.

--- test.py
+++ test.py
@@ -1,9 +1,34 @@
+__all__ = [
+    'matrix'
+]
+
 import random
 import pandas as pd
 import numpy as np
 
 
-def test(cols_size, rows_size):
+def matrix(cols_size: int, rows_size: int) -> 'pd.DataFrame':
+    """
+    Generate matrix of random numbers between 0 and 100 of size passed to
+    function arguments.
+
+    Parameters
+    ----------
+    cols_size: int, required
+        Number of columns in matrix.
+    rows_size: int, required
+        Number of rows in matrix.
+
+    Returns
+    -------
+    Function returns matrix of dimension cols_size x rows_size,
+    filled with random generated numbers. The matrix is instance of pandas'
+    DataFrame object.
+
+    Example
+    -------
+    >>> array = matrix(5,5)
+    """
     table = []
 
     for i in range(rows_size):
@@ -11,7 +36,5 @@ def test(cols_size, rows_size):
 
     x = np.array(table, np.int32)
     df = pd.DataFrame(x, index=range(rows_size), columns=range(cols_size))
-    print(df)
 
-
-test(10, 10)
+    return df
